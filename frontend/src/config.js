129b33f85f8662b1ec4551d1ce5c2210a43f15e5
belalbahaa1 <belalbahaa800@gmail.com>
2026-02-12 13:32:17 +0200

    update frontend config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,5 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+  import.meta.env.VITE_API_BASE_URL ||
+  "https://book-store-ruddy-one.vercel.app";
 
 export default API_BASE_URL;
